0aa67e8513a133a2c7411279dae241437c2379cc
Ishita L <79676329+NPC-Bl00d1@users.noreply.github.com>
2022-06-13 10:40:21 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+nosex = 0;
+nosey = 0;
+
+rightWrist = 0;
+leftWrist = 0;
+difference = 0;
+
 function setup(){
 
 video = createCapture(VIDEO);
@@ -14,6 +21,12 @@ function draw(){
 
 background('#ceced6');
 
+document.getElementById("square_side").innerHTML = "Width and Height of the square will be = " + difference + "px";
+
+fill('#865d8f');
+stroke('#865d8f');
+square(nosex, nosey, difference);
+
 }
 
 function modelLoaded(){
@@ -26,5 +39,14 @@ function gotPoses(results){
 
 if(results.length > 0){
     console.log(results);
+
+    nosex = results[0].pose.nose.x;
+    nosey = results[0].pose.nose.y;
+    console.log("nose x =" + nosex + "and nose y =" + nosey);
+
+    rightWrist = results[0].pose.rightWrist.x;
+    leftWrist = results[0].pose.leftWrist.y;
+    difference = floor(leftWrist - rightWrist);
+    console.log("left wrist x =" + leftWrist + "right wrist x = " + rightWrist + "difference = " + difference);
 }
 }
